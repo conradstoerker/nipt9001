f4ad6db8eb52d9245dae8bd637f12115b58ef888
conradstoerker <conrad.stoerker@progenity.com>
2015-06-19 08:16:11 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ cp /mnt/scratch/devel/NIPT9001/echelon_src/2015-06-06WholeChr/*.pm /usr/lib64/pe
 PERL5LIB=/mnt/scratch/devel/NIPT9001/dependencies/bcl2fastq/lib/bcl2fastq-1.8.3/perl:$PERL5LIB; export PERL5LIB
 
 #Command changed to run in the container
-nohup /mnt/scratch/devel/NIPT9001/dependencies/anaconda/bin/python /mnt/scratch/devel/NIPT9001/src/NIPT9001_process_NGSrun_pyflow.py -c /mnt/scratch/devel/NIPT9001/config/docker_config.xml -r /mnt/production_storage/runs/hiseq/150423_COUNT_0015_AC6V7GANXX > /mnt/production_storage/workflows/docker/150423_COUNT_0015_AC6V7GANXX_20150618.out
+nohup /mnt/scratch/devel/NIPT9001/dependencies/anaconda/bin/python /mnt/scratch/devel/NIPT9001/src/NIPT9001_process_NGSrun_pyflow.py -c /mnt/scratch/devel/NIPT9001/config/docker_config.xml -r /mnt/production_storage/runs/hiseq/150423_COUNT_0015_AC6V7GANXX > /mnt/production_storage/workflows/docker/150423_COUNT_0015_AC6V7GANXX_20150619.out
 #nohup /mnt/scratch/devel/NIPT9001/dependencies/anaconda/bin/python /mnt/scratch/devel/NIPT9001/src/NIPT9001_process_NGSrun_pyflow.py -c /mnt/scratch/devel/NIPT9001/config/docker_config.xml -r /mnt/production_storage/runs/hiseq/150423_COUNT_0015_AC6V7GANXX > /mnt/production_storage/workflows/docker/${DATANAME}_${DATETAG}.out
